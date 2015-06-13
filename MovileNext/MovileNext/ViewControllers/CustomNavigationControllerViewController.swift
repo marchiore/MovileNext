bf55ddc30e33b1b6e340b0b6ada441b2fc4e9bd1
User <User@MAC-128.nabs.com.br>
2015-06-13 16:48:25 -0300

    First Commit

---
+++ MovileNext/MovileNext/ViewControllers/CustomNavigationControllerViewController.swift
@@ -0,0 +1,17 @@
+//
+//  CustomNavigationControllerViewController.swift
+//  MovileNext
+//
+//  Created by User on 13/06/15.
+//  Copyright (c) 2015 Movile. All rights reserved.
+//
+
+import UIKit
+
+class CustomNavigationControllerViewController: UINavigationController {
+
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+        return .LightContent
+    }
+
+}
---
+++ MovileNext/MovileNext/ViewControllers/EpisodeViewController.swift
@@ -0,0 +1,47 @@
+//
+//  EpisodeViewController.swift
+//  MovileNext
+//
+//  Created by User on 13/06/15.
+//  Copyright (c) 2015 Movile. All rights reserved.
+//
+
+import UIKit
+
+class EpisodeViewController: UIViewController {
+
+    @IBOutlet weak var labelEpisodio: UILabel!
+    @IBOutlet weak var textEpisodio: UITextView!
+    @IBOutlet weak var imageEpisodio: UIImageView!
+    
+    @IBAction func sharePressed(sender: UIBarButtonItem) {
+
+        let url = NSURL(string: "http://www.apple.com")!
+        let vc = UIActivityViewController(activityItems: [url], applicationActivities: nil)
+        
+        presentViewController(vc, animated: true, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
